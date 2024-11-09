566f0f2d8f18f4757c9a17f0a7df73dc087ec9b5
NatiOzawa <nati.ozawa@gmail.com>
2024-11-09 16:21:32 -0300

    Cria show mentoras

--- app/controllers/mentoras_controller.rb
+++ app/controllers/mentoras_controller.rb
@@ -2,4 +2,10 @@ class MentorasController < ApplicationController
   def index
     @mentoras = Mentora.all
   end
+
+  def show
+    # puts params
+    @mentora = Mentora.find(params[:id])
+    # puts @mentora.inspect
+  end
 end
